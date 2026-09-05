6b79701a9aa021edd8d93c17caacd1a867d7120e
Saad Shaikh <msaadbjs7@gmail.com>
2026-09-05 10:52:50 +0530

    feat: add department and job position APIs

--- server/routes/jobPositionRoutes.js
+++ server/routes/jobPositionRoutes.js
@@ -2,11 +2,14 @@ const express = require('express');
 const router = express.Router();
 const jobPositionController = require('../controllers/jobPositionController');
 const { authenticate, authorize } = require('../middleware/auth');
+const { HR_MANAGERS, ADMIN_ONLY } = require('../config/roles');
 
 router.use(authenticate);
 
 router.get('/', jobPositionController.getJobPositions);
 router.get('/:id', jobPositionController.getJobPositionById);
-router.post('/', authorize('Admin', 'HR'), jobPositionController.createJobPosition);
+router.post('/', authorize(...HR_MANAGERS), jobPositionController.createJobPosition);
+router.put('/:id', authorize(...HR_MANAGERS), jobPositionController.updateJobPosition);
+router.delete('/:id', authorize(...ADMIN_ONLY), jobPositionController.deleteJobPosition);
 
 module.exports = router;
